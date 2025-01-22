eefc39d42b907a0241f25c218f3a8f135891eea2
Kai Goette <kai.goe@outlook.de>
2025-01-22 21:24:17 +0100

    magic with mobs

--- src/main/java/de/kaigoette/pluginKai/MagicStick/MagicStickListener.java
+++ src/main/java/de/kaigoette/pluginKai/MagicStick/MagicStickListener.java
@@ -3,10 +3,13 @@ package de.kaigoette.pluginKai.MagicStick;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
@@ -41,6 +44,23 @@ public class MagicStickListener implements Listener {
                     block.getWorld().spawnParticle(Particle.BLOCK, block.getLocation().add(new Vector(0.5, 1, 0.5)), 10, new Particle.DustOptions(org.bukkit.Color.fromRGB((int) (red * 255), (int) (green * 255), (int) (blue * 255)), 1));
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (new MagicStickItem().equals(item)) {
+            Entity entity = event.getRightClicked();
+            EntityType[] entityTypes = EntityType.values();
+            Random random = new Random();
+            EntityType randomEntityType;
+            do {
+                randomEntityType = entityTypes[random.nextInt(entityTypes.length)];
+            } while (!randomEntityType.isSpawnable() || randomEntityType == entity.getType());
+            entity.getWorld().spawnEntity(entity.getLocation(), randomEntityType);
+            entity.remove();
         }
     }
 }
