023536d71056098fde4afa33873c0362d925d1d4
Kai Goette <kai.goe@outlook.de>
2025-01-29 15:56:20 +0100

    moved world switcher fuctions to worldswitchermain

--- src/main/java/de/kaigoette/pluginKai/worldSwitcher/WorldSwitcherMain.java
+++ src/main/java/de/kaigoette/pluginKai/worldSwitcher/WorldSwitcherMain.java
@@ -1,6 +1,8 @@
 package de.kaigoette.pluginKai.worldSwitcher;
 
 import de.kaigoette.pluginKai.Main;
+import org.bukkit.WorldCreator;
+import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.util.*;
@@ -57,5 +59,102 @@ public class WorldSwitcherMain {
     public void setWorlds(List<World> worlds) {
         this.worlds.clear();
         this.worlds.addAll(worlds);
+    }
+
+    public boolean invitePlayer(Player player, String worldName, String playerName) {
+        World world = this.getWorlds().stream()
+                .filter(w -> w.name().equals(worldName) && w.members().get(0).equals(player.getName()))
+                .findFirst()
+                .orElse(null);
+        if(world != null) {
+            world.members().add(playerName);
+            player.sendMessage("Invited " + playerName + " to world " + worldName);
+            player.getServer().getOnlinePlayers().stream()
+                    .filter(p -> p.getName().equals(playerName))
+                    .findFirst()
+                    .ifPresent(p -> p.sendMessage("You were invited to world " + worldName));
+            return true;
+        } else {
+            player.sendMessage("World not found!");
+        }
+        return false;
+    }
+
+    public boolean joinWorld(Player player, String worldName) {
+        World joinWorld = this.getWorlds().stream()
+                .filter(w -> w.name().equals(worldName))
+                .findFirst()
+                .orElse(null);
+        if (joinWorld != null && (joinWorld.members().contains(player.getName()) || joinWorld.name().equals("world") || joinWorld.name().equals("world_nether") || joinWorld.name().equals("world_the_end"))) {
+            player.teleport(player.getServer().getWorld(joinWorld.name()).getSpawnLocation());
+            player.sendMessage("You joined the world " + joinWorld.name());
+            return true;
+        } else {
+            player.sendMessage("World not found!");
+        }
+        return false;
+    }
+
+    public boolean addWorld(Player player, String worldName, String  seed) {
+        if(player.getServer().getWorlds().stream()
+                .anyMatch(world -> world.getName().equals(worldName))) {
+            player.sendMessage("World already exists!");
+            return false;
+        }
+        World addedWorld = new World(worldName, new ArrayList<>());
+        addedWorld.members().add(player.getName());
+        this.getWorlds().add(addedWorld);
+        player.sendMessage("Creating world...");
+        if (seed != null) {
+            try {
+                player.getServer().createWorld(new WorldCreator(worldName).seed(Long.parseLong(seed)));
+            } catch (NumberFormatException e) {
+                player.sendMessage("Invalid seed!");
+                player.getServer().createWorld(new WorldCreator(worldName));
+                return false;
+            }
+        } else {
+            player.getServer().createWorld(new WorldCreator(worldName));
+        }
+        player.sendMessage("World added!");
+        return true;
+    }
+
+    public boolean kickPlayer(Player player, String worldName, String playerName) {
+        World world = this.getWorlds().stream()
+                .filter(w -> w.name().equals(worldName) && w.members().get(0).equals(player.getName()))
+                .findFirst()
+                .orElse(null);
+        if(world != null) {
+            if(world.members().remove(playerName)) {
+                player.sendMessage("Kicked " + playerName + " from world " + worldName);
+                player.getServer().getOnlinePlayers().stream()
+                        .filter(p -> p.getName().equals(playerName))
+                        .findFirst()
+                        .ifPresent(p -> p.sendMessage("You were kicked from world " + worldName));
+                return true;
+            } else {
+                player.sendMessage("Player is not a member!");
+            }
+        } else {
+            player.sendMessage("World not found!");
+        }
+        return false;
+    }
+
+    public boolean removeWorld(Player player, String worldName) {
+        World removeWorld = this.getWorlds().stream()
+                .filter(world ->
+                        (!world.members().isEmpty() && world.members().contains(player.getName()))
+                                && world.name().equals(worldName)
+                )
+                .findFirst().orElse(null);
+        if(removeWorld != null) {
+            this.getWorlds().remove(removeWorld);
+            player.teleport(player.getServer().getWorld("world").getSpawnLocation());
+            player.sendMessage("Removed your world!");
+            return true;
+        }
+        return false;
     }
 }
